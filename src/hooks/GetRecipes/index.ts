8f118f5ed517fca7d39ae1013c0a205ff54d31a7
Marcio Tiene <marciorft@gmail.com>
2020-10-05 18:23:10 -0300

    type check and some refactor

--- src/hooks/GetRecipes/index.ts
+++ src/hooks/GetRecipes/index.ts
@@ -4,35 +4,36 @@ import { getRecipes } from '../../repositories/Recipes';
 import { Recipes } from '../../types';
 
 const GetRecipes = () => {
-  const [recipesRaw, setRecipesRaw] = useState<Recipes[]>([{ updated_at: '' }]);
-  const [recipes, setRecipes] = useState(recipesRaw);
-  //getting recipes from Db
+  const [recipes, setRecipes] = useState<Recipes[]>([{ updated_at: '' }]);
+
   useEffect(() => {
     getRecipes()
       .then((recipeDbData) => {
-        setRecipesRaw(recipeDbData);
+        const recipeSortedByDate = recipeDbData.sort(
+          (recipe_01: Recipes, recipe_02: Recipes) => {
+            const date_01InMs = new Date(`${recipe_01.updated_at}`).getTime();
+            const date_02InMs = new Date(`${recipe_02.updated_at}`).getTime();
+
+            return date_02InMs - date_01InMs;
+          }
+        );
+
+        setRecipes(recipeSortedByDate);
       })
       .catch((err) => {
         console.log(err.message);
       });
   }, []);
-  // sorting recipes by updated_at
-  useEffect(() => {
-    setRecipes(
-      recipesRaw.sort((recipe_01: Recipes, recipe_02: Recipes) => {
-        const date1InMs = new Date(`${recipe_01.updated_at}`).getTime();
-        const date2InMs = new Date(`${recipe_02.updated_at}`).getTime();
 
-        return date2InMs - date1InMs;
-      })
-    );
-  }, [recipesRaw]);
+  const recipesLastUpate = new Date(
+    Math.max(
+      ...recipes.map((recipe: Recipes) =>
+        new Date(`${recipe.updated_at}`).getTime()
+      )
+    )
+  ).toLocaleDateString();
 
-  // const recipeUpated =  new Date(
-  //   Math.max(...recipes.map((recipe: any) => new Date(recipe.updated_at)))
-  // ).toLocaleDateString();
-
-  return [recipes];
+  return [recipes, recipesLastUpate];
 };
 
 export default GetRecipes;
