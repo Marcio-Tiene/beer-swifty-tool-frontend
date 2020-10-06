5358db3a445cf6ecd26e1f53ede229c07295680e
Wesley <wes.bnvnd@gmail.com>
2020-10-05 23:49:35 -0300

    axios added

--- src/hooks/GetRecipes/index.ts
+++ src/hooks/GetRecipes/index.ts
@@ -1,34 +1,36 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
-import { getRecipes } from '../../repositories/Recipes';
-import { Recipes } from '../../types';
+// import { getRecipes } from '../../repositories/Recipes';
+
+import api from "../../services/api";
+
+import { Recipes } from "../../types";
 
 const GetRecipes = () => {
-  const [recipes, setRecipes] = useState<Recipes[]>([]);
+    const [recipes, setRecipes] = useState<Recipes[]>([]);
 
-  function SortRecipesByDate(recipes: Recipes[]) {
-    recipes.sort((recipe_01: Recipes, recipe_02: Recipes) => {
-      const date_01InMs = new Date(`${recipe_01.updated_at}`).getTime();
-      const date_02InMs = new Date(`${recipe_02.updated_at}`).getTime();
+    function SortRecipesByDate(recipes: Recipes[]) {
+        recipes.sort((recipe_01: Recipes, recipe_02: Recipes) => {
+            const date_01InMs = new Date(`${recipe_01.updated_at}`).getTime();
+            const date_02InMs = new Date(`${recipe_02.updated_at}`).getTime();
 
-      return date_02InMs - date_01InMs;
-    });
-    return recipes;
-  }
+            return date_02InMs - date_01InMs;
+        });
+        return recipes;
+    }
 
-  useEffect(() => {
-    getRecipes()
-      .then((recipeDbData) => {
-        const recipeSortedByDate = SortRecipesByDate(recipeDbData);
+    useEffect(() => {
+        (async function loadRecipes(): Promise<void> {
+            try {
+                const response = await api.get("/recipes");
+                setRecipes(response.data);
+            } catch (err) {
+                console.log("algo de errado não está certo");
+            }
+        })();
+    }, []);
 
-        setRecipes(recipeSortedByDate);
-      })
-      .catch((err) => {
-        console.log(err.message);
-      });
-  }, []);
-
-  return [recipes];
+    return [recipes];
 };
 
 export default GetRecipes;
